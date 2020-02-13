a13c1f1ffd733bc5858e4a3124a9533ea52b8f63
Akhila <akhilavanam@Akhilas-MacBook-Pro.local>
2020-02-13 21:14:33 +0530

    Proxy design pattern

---
+++ Proxy/Proxy.java
@@ -0,0 +1,55 @@
+// Proxy controlls the access to object.
+// RemoteProxy & VirtualProxy
+
+class ImageIconProxy implements Icon {
+	ImageIcon imageIcon;
+	Url url;
+	Thread retreivalThread;
+	boolean retrieving = false;
+
+	public ImageIconProxy(Url url) {
+		this.url = url;
+	}
+
+	public double getWidth() {
+		if( imageIcon != null ) {
+			return imageIcon.getWidth();
+		}
+		else {
+			return 800;
+		}
+	}
+
+	public double getHeight() {
+		if( imageIcon != null ) {
+			return imageIcon.getHeight();
+		}
+		else {
+			return 600;
+		}
+	}
+
+	public void setImageIcon(ImageIcon imageIcon) {
+		this.imageIcon = imageIcon;
+	}
+
+	public void paint( final Componenet c, Graphics g, int x, int y) {
+		if (imageIcon != null)  {
+			imageIcon.paintIcon(c,g,x,y);
+		}
+		else {
+			g.drawString("PLeae wait...", x+300, y+200);
+			if(!retrieving) {
+				retreivalThread = new Thread(new Runnable(){
+					public void run() {
+						setImageIcon(new ImageIcon(url));
+						c.repaint();
+					}
+				});
+				retrieving = true;
+				retreivalThread.start();
+			}
+		}
+
+	}
+}
